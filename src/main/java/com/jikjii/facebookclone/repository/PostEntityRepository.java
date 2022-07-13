35106718c156017b9fb5fd66896b477f41ac9f67
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-07-13 18:34:38 -0400

    Misc

---
+++ src/main/java/com/jikjii/facebookclone/repository/PostEntityRepository.java
@@ -0,0 +1,9 @@
+package com.jikjii.facebookclone.repository;
+
+import com.jikjii.facebookclone.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostEntityRepository extends JpaRepository<PostEntity,String> {
+}
